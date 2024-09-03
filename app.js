71828c4aed70b66c1cfb964291b18a43ab18f256
JvitorAfb2004 <jvitorafb@gmail.com>
2024-09-03 12:14:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
+// Configuração do aplicativo
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-const axios = require('axios');
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
+// Carregar certificados SSL
+const privateKey = fs.readFileSync('private.key', 'utf8');
+const certificate = fs.readFileSync('certificate.crt', 'utf8');
+const caBundle = fs.readFileSync('ca_bundle.crt', 'utf8');
 
+const credentials = { key: privateKey, cert: certificate, ca: caBundle };
 
-
-
-
+// Rota para calcular a distância
 app.get('/api/distance', async (req, res) => {
   try {
     const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json`, {
@@ -30,13 +36,7 @@ app.get('/api/distance', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-
-// Endpoint para receber os dados via GET e encaminhar para a API
+// Endpoint para gerar QR Code
 app.get('/gerarqrcode', async (req, res) => {
   try {
     const { nome, cidade, valor, saida, txid, chave } = req.query;
@@ -59,3 +59,26 @@ app.get('/gerarqrcode', async (req, res) => {
     res.status(500).json({ error: 'Erro ao gerar QR Code PIX.' });
   }
 });
+
+// Função para encontrar uma porta aleatória disponível
+function findRandomPort() {
+  return new Promise((resolve, reject) => {
+    const server = https.createServer(credentials, app);
+    server.listen(0, () => {
+      const port = server.address().port;
+      server.close(() => resolve(port));
+    });
+    server.on('error', reject);
+  });
+}
+
+// Inicia o servidor HTTPS na porta aleatória
+findRandomPort()
+  .then(port => {
+    https.createServer(credentials, app).listen(port, () => {
+      console.log(`Servidor HTTPS rodando na porta ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao encontrar uma porta aleatória:', err);
+  });
